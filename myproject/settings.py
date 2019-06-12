629d5d2638bf4bb84f91a18fe82342ed5ec00408
KimYeeun99 <kyz0608@naver.com>
2019-06-12 19:55:00 +0900

    commit2

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','emct88ho5b0kw+dts^rhz(akxyh2szo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
